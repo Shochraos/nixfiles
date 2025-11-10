f9723a0a86913a5786b0fbbfb4720d09f59b75e2
Shochraos <github@shonline.slmail.me>
2025-11-10 13:23:39 +0100

    typo

--- home/modules/mpv.nix
+++ home/modules/mpv.nix
@@ -32,6 +32,6 @@
     };
   };
 
-  # jellyfin-mpv-shim config to use external mpv
+  # Jellyfin-mpv-shim config to use external mpv
   xdg.configFile."jellyfin-mpv-shim/conf.json".source = ../../configs/jellyfin-mpv-shim/conf.json;
 }
