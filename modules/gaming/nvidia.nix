f1cae100aaf522d286b3c2529fa4303eea24b10a
Shochraos <github@raiton.net>
2026-02-16 00:26:11 +0100

    nvidia 6.19 fix

--- modules/gaming/nvidia.nix
+++ modules/gaming/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ inputs, config, ... }:
 {
   hardware.graphics =
   {
@@ -14,6 +14,15 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package =
+          let
+            nvidia-fixed-pkgs = import inputs.nixpkgs-nvidia {
+              system = "x86_64-linux";
+              config.allowUnfree = true;
+            };
+            fixedKernelPackages = nvidia-fixed-pkgs.linuxKernel.packagesFor config.boot.kernelPackages.kernel;
+          in
+          fixedKernelPackages.nvidiaPackages.stable;
   };
 }
