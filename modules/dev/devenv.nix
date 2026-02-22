4a4bcc26d1e51dd0a982a110a218ce341474f3c9
Shochraos <github@shonline.slmail.me>
2026-02-22 22:12:31 +0100

    adjusted devenv to correctly read public key from input file

--- modules/dev/devenv.nix
+++ modules/dev/devenv.nix
@@ -2,7 +2,10 @@
 {
   nix = 
   {
-    extraOptions = lib.lists.remove "" (lib.strings.splitString "\n" (builtins.readFile ../../local/devenv.txt));
+    extraOptions = ''
+      extra-substituters = https://devenv.cachix.org
+      extra-trusted-public-keys = ${lib.removeSuffix "\n" (builtins.readFile ../../local/devenv.txt)}
+    '';
   };
   
   home-manager.users.${username} =
