a3a63c32d7b4566c7fdab6d212de3bbee5aef4a4
Shochraos <github@raiton.net>
2026-02-28 15:12:21 +0100

    Prebroken Audio

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -1,5 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [ rnnoise-plugin ];
+  
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = 
@@ -7,5 +9,49 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    
+    extraConfig.pipewire."99-input-denoising" = 
+    {
+      "context.modules" = 
+      [
+        {
+          name = "libpipewire-module-filter-chain";
+          args = {
+            "node.description" = "RNNoise";
+            "media.name" = "RNNoise";
+            "filter.graph" = 
+            {
+              nodes = 
+              [
+                {
+                  type = "ladspa";
+                  name = "rnnoise";
+                  plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                  label = "noise_suppressor_mono";
+                  control = 
+                  {
+                    "VAD Threshold (%)" = 50.0;
+                    "VAD Grace Period (ms)" = 200;
+                    "Retroactive VAD Grace (ms)" = 0;
+                  };
+                }
+              ];
+            };
+            "capture.props" = 
+            {
+              "node.name" = "capture.rnnoise_source";
+              "node.passive" = true;
+              "audio.rate" = 48000;
+            };
+            "playback.props" = 
+            {
+              "node.name" = "rnnoise_source";
+              "media.class" = "Audio/Source";
+              "audio.rate" = 48000;
+            };
+          };
+        }
+      ];
+    };
   };
 }
