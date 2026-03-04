58288d8792c5d584f9ec12daebc8f8ab85019e2a
Shochraos <github@shonline.slmail.me>
2026-03-04 14:52:00 +0100

    adjusted nfs settings and fixed formatting

--- modules/addons/nfs.nix
+++ modules/addons/nfs.nix
@@ -1,44 +1,51 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [ rsync ];
+  
   boot.supportedFilesystems = [ "nfs" ];
   services.rpcbind.enable = true;
-
-  systemd.mounts = let commonMountOptions = {
+  
+  systemd.mounts = let commonMountOptions = 
+  {
     type = "nfs";
-    mountConfig = {
-      Options = "noatime";
+    mountConfig = 
+    {
+      Options = "nfsvers=4.2,soft,async,noatime,timeo=50,retrans=5,actimeo=60,rsize=1048576,wsize=1048576";
     };
   };
 
   in
 
   [
-    (commonMountOptions // {
+    (commonMountOptions // 
+      {
       what = "192.168.10.2:/mnt/user/appdata";
       where = "/mnt/astaroth/appdata";
     })
 
-    (commonMountOptions // {
+    (commonMountOptions // 
+      {
       what = "192.168.10.2:/mnt/user/backups";
       where = "/mnt/astaroth/backups";
     })
     
-    (commonMountOptions // {
+    (commonMountOptions // 
+      {
       what = "192.168.10.2:/mnt/user/data";
       where = "/mnt/astaroth/data";
     })
     
-    (commonMountOptions // {
+    (commonMountOptions // 
+      {
       what = "192.168.10.2:/mnt/user/misc";
       where = "/mnt/astaroth/misc";
     })
   ];
 
-  systemd.automounts = let commonAutoMountOptions = {
+  systemd.automounts = let commonAutoMountOptions = 
+  {
     wantedBy = [ "multi-user.target" ];
-    automountConfig = {
-      TimeoutIdleSec = "600";
-    };
+    automountConfig = { TimeoutIdleSec = "600"; };
   };
   in
   [
