972858689f9cebaf4c245a78b89f567e94b3d87c
Shochraos <github@shonline.slmail.me>
2026-03-06 11:23:57 +0100

    replaced nfs with sshfs due to stability

--- hosts/Azazel/configuration.nix
+++ hosts/Azazel/configuration.nix
@@ -12,7 +12,7 @@
     ../../modules/addons/cloud.nix
     ../../modules/addons/common-packages.nix
     ../../modules/addons/mpv.nix
-    ../../modules/addons/nfs.nix
+    ../../modules/addons/sshfs.nix
     ../../modules/addons/office.nix
     ../../modules/addons/scanprint.nix
     
---
+++ modules/addons/sshfs.nix
@@ -0,0 +1,68 @@
+{ pkgs, lib, username, systemname, ... }:
+{
+  environment.systemPackages = with pkgs; [ rclone rsync ];
+  environment.etc."rclone-mnt.conf".text = 
+  ''
+    [astaroth]
+    type = sftp
+    host = 192.168.10.2
+    user = root
+    key_file = /home/${username}/.ssh/${lib.toLower systemname}
+  '';
+  
+  fileSystems."/mnt/astaroth/appdata" = 
+  {
+    device = "astaroth:/mnt/user/appdata";
+    fsType = "rclone";
+    options = 
+    [
+      "nodev"
+      "nofail"
+      "allow_other"
+      "args2env"
+      "config=/etc/rclone-mnt.conf"
+    ];
+  };
+  
+  fileSystems."/mnt/astaroth/backups" = 
+  {
+    device = "astaroth:/mnt/user/backups";
+    fsType = "rclone";
+    options = 
+    [
+      "nodev"
+      "nofail"
+      "allow_other"
+      "args2env"
+      "config=/etc/rclone-mnt.conf"
+    ];
+  };
+  
+  fileSystems."/mnt/astaroth/data" = 
+  {
+    device = "astaroth:/mnt/user/data";
+    fsType = "rclone";
+    options = 
+    [
+      "nodev"
+      "nofail"
+      "allow_other"
+      "args2env"
+      "config=/etc/rclone-mnt.conf"
+    ];
+  };
+  
+  fileSystems."/mnt/astaroth/misc" = 
+  {
+    device = "astaroth:/mnt/user/misc";
+    fsType = "rclone";
+    options = 
+    [
+      "nodev"
+      "nofail"
+      "allow_other"
+      "args2env"
+      "config=/etc/rclone-mnt.conf"
+    ];
+  };
+}
