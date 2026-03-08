5f3c18b78e81c5f2d7bedb02b520003de9888835
Shochraos <github@shonline.slmail.me>
2026-03-08 18:12:30 +0100

    adjusted mangohud config to accommodate 144hz instead of 120hz

--- modules/gaming/mangohud.nix
+++ modules/gaming/mangohud.nix
@@ -1,6 +1,6 @@
 { username, ... }:
 {
-  home-manager.users.${username} = 
+  home-manager.users.${username} =
   {
     programs.mangohud =
     {
@@ -8,23 +8,23 @@
       settings =
       {
         fps = true;
-        fps_limit = 117;
-  
+        fps_limit = 141;
+
         cpu_temp = true;
         cpu_mhz = true;
         cpu_power = true;
-  
+
         gpu_temp = true;
         gpu_core_clock = true;
         gpu_mem_clock = true;
         gpu_power = true;
-  
+
         no_display = true;
         background_alpha = 0;
         winesync = true;
       };
     };
-    
+
     home.sessionVariables =
     {
       MANGOHUD = "1";
