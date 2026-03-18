91180fe0ff73863f2dcb5a1ada1138bf3d81bb64
Shochraos <github@raiton.net>
2026-03-18 21:52:38 +0100

    refactor

--- modules/utilities/gamechat.nix
+++ modules/utilities/gamechat.nix
@@ -5,44 +5,14 @@
   
   home-manager.users.${username} =
   {
-    xdg.desktopEntries =
+    wayland.windowManager.hyprland.settings = 
     {
-      "gamechat_chat" =
-      {
-        name = "gamechat_chat";
-        exec = "/home/${username}/Scripts/Pipewire/gamechat_chat.sh";
-        terminal = false;
-        startupNotify = false;
-        noDisplay = true;
-      };
-  
-      "gamechat_game" =
-      {
-        name = "gamechat_game";
-        exec = "/home/${username}/Scripts/Pipewire/gamechat_game.sh";
-        terminal = false;
-        startupNotify = false;
-        noDisplay = true;
-      };
-  
-      "gamechat_reset" =
-      {
-        name = "gamechat_reset";
-        exec = "/home/${username}/Scripts/Pipewire/gamechat_reset.sh";
-        terminal = false;
-        startupNotify = false;
-        noDisplay = true;
-      };
-    };
-    
-    programs.plasma = 
-    {
-      shortcuts =
-      {
-        "services/gamechat_chat.desktop"._launch = "Volume Down";
-        "services/gamechat_game.desktop"._launch = "Volume Up";
-        "services/gamechat_reset.desktop"._launch = "Ctrl+Shift+D";
-      };
+      bind = 
+      [
+        ", XF86AudioRaiseVolume, exec, /home/${username}/Scripts/Pipewire/gamechat_game.sh"
+        ", XF86AudioLowerVolume, exec, /home/${username}/Scripts/Pipewire/gamechat_chat.sh"
+        "CTRL SHIFT, D, exec, /home/${username}/Scripts/Pipewire/gamechat_reset.sh"
+      ];
     };
   };
 }
