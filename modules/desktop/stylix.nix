09b7c0370849cb57c21462920344bc8b1db7320a
Shochraos <github@raiton.net>
2026-03-19 20:34:19 +0100

    theme refactor

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -4,7 +4,7 @@
   {
     enable = true;
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    base16Scheme = ../../assets/themes/metro.yaml;
+    base16Scheme = ../../assets/themes/adwaita.yaml;
     
     cursor = 
     {
