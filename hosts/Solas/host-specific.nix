1f385a4f082970b7625d9414050462451a574bd5
Shochraos <github@shonline.slmail.me>
2026-03-19 21:17:48 +0100

    added Solas device config

---
+++ hosts/Solas/host-specific.nix
@@ -0,0 +1,27 @@
+{ username,... }:
+{
+  services.fprintd.enable = true;
+  services.upower.enable = true;
+  
+  services.xserver.xkb =
+  {
+    layout = "us";
+    variant = "altgr-intl";
+  };
+  console.keyMap = "us";
+  
+  home-manager.users.${username} = 
+  {
+    wayland.windowManager.hyprland = 
+    {
+      settings = 
+      {       
+        input = 
+        {
+          kb_layout = "us"; 
+          kb_variant = "altgr-intl";
+        };
+      };
+    };
+  };
+}
