b8fb6e6cff1e01a309610b3c4cc75c777cbab40a
Shochraos <github@raiton.net>
2026-03-22 15:52:21 +0100

    added brightness and volume controls to Solas

--- hosts/Solas/host-specific.nix
+++ hosts/Solas/host-specific.nix
@@ -1,4 +1,4 @@
-{ username,... }:
+{ pkgs, username,... }:
 {
   services.fprintd.enable = true;
   services.upower.enable = true;
@@ -12,6 +12,11 @@
   
   home-manager.users.${username} = 
   {
+    home.packages = with pkgs;
+    [
+      brightnessctl
+    ];
+    
     wayland.windowManager.hyprland = 
     {
       settings = 
@@ -36,6 +41,21 @@
         {
           workspace_swipe_invert = true;
         };
+        
+        binde = 
+        [
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          
+          ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+          ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ];
+        
+        bindel = 
+        [
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ];
       };
     };
   };
