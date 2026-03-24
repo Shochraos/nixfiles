e9a3298601b4d208567625b397454c6a4dd41da3
Shochraos <github@shonline.slmail.me>
2026-03-24 22:55:06 +0100

    added nix helper garbage cleaner

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -10,13 +10,6 @@
       automatic = true;
       dates = [ "daily" ];
     };
-    
-    gc = 
-    {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 7d";
-    };
   };
   
   systemd.timers."nix-gc.timer".timerConfig =
