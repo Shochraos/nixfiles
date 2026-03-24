d944243369e2d11cfbc63cbe336e847937105013
Shochraos <github@raiton.net>
2026-03-24 22:55:06 +0100

    added nix helper garbage cleaner

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -28,8 +28,8 @@
     programs.nh = 
     {
       enable = true;
-      clean.enable = false;
-      clean.extraArgs = "--keep-since 4d --keep 3";
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 7d --keep 10";
     };
     
     programs.fish =
