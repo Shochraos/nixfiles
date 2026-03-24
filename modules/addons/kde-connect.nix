369ae0c306c894f1ee41c18cb08e96853e641874
Shochraos <github@raiton.net>
2026-03-24 21:53:38 +0100

    moved kde to own module

---
+++ modules/addons/kde-connect.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  programs.kdeconnect =
+  {
+    enable = true;
+    package = pkgs.valent;
+  };
+  
+  systemd.user.services.valent = 
+  {
+    description = "Valent (KDE Connect Implementation)";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.valent}/bin/valent --gapplication-service";
+      Restart = "on-failure";
+    };
+  };
+}
--- modules/desktop/dms.nix
+++ modules/desktop/dms.nix
@@ -7,23 +7,6 @@
     configHome = "/home/${username}";
   };
   
-  programs.kdeconnect =
-  {
-    enable = true;
-    package = pkgs.valent;
-  };
-  
-  systemd.user.services.valent = 
-  {
-    description = "Valent (KDE Connect Implementation)";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.valent}/bin/valent --gapplication-service";
-      Restart = "on-failure";
-    };
-  };
-  
   home-manager.users.${username} = 
   {
     imports = 
