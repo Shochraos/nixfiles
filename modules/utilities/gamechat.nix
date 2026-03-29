2935832f31dacc44a45762db542ebe3af573388c
Shochraos <github@shonline.slmail.me>
2026-03-29 14:46:19 +0200

    made the gamechat configuration declarative

--- modules/utilities/gamechat.nix
+++ modules/utilities/gamechat.nix
@@ -1,4 +1,10 @@
 { pkgs, username, ... }:
+let
+  gamechat_mix = pkgs.writeShellScript "gamechat_mix.sh" (builtins.readFile ../../assets/scripts/gamechat_mix.sh);
+  gamechat_chat = pkgs.writeShellScript "gamechat_chat.sh" (builtins.readFile ../../assets/scripts/gamechat_chat.sh);
+  gamechat_game = pkgs.writeShellScript "gamechat_game.sh" (builtins.readFile ../../assets/scripts/gamechat_game.sh);
+  gamechat_reset = pkgs.writeShellScript "gamechat_reset.sh" (builtins.readFile ../../assets/scripts/gamechat_reset.sh);
+in
 {
   services.pipewire.pulse.enable = true;
   environment.systemPackages = with pkgs; [ pulseaudio ];
@@ -9,10 +15,22 @@
     {
       bind = 
       [
-        ", XF86AudioRaiseVolume, exec, /home/${username}/Scripts/Pipewire/gamechat_game.sh"
-        ", XF86AudioLowerVolume, exec, /home/${username}/Scripts/Pipewire/gamechat_chat.sh"
-        "CTRL SHIFT, D, exec, /home/${username}/Scripts/Pipewire/gamechat_reset.sh"
+        ", XF86AudioRaiseVolume, exec, ${gamechat_game}"
+        ", XF86AudioLowerVolume, exec, ${gamechat_chat}"
+        "CTRL SHIFT, D, exec, ${gamechat_reset}"
       ];
     };
+    
+    xdg.autostart.entries = 
+    [
+      "${pkgs.makeDesktopItem 
+      {
+        name = "gamechat_mix";
+        desktopName = "GameChat-Mix";
+        exec = "${gamechat_mix}";
+        noDisplay = true;
+      }
+      }/share/applications/gamechat_mix.desktop"
+    ];
   };
 }
