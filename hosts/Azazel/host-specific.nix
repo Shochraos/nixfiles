a64a7aa123ac42748dda9e64654c502fbc674492
Shochraos <github@raiton.net>
2026-03-29 15:11:02 +0200

    moved lgtv configuration to own module and added a service to turn the screen off as well as added a keybind to turn the screen on manually

--- hosts/Azazel/host-specific.nix
+++ hosts/Azazel/host-specific.nix
@@ -1,22 +1,5 @@
 { pkgs, username, ... }:
 {
-  environment.systemPackages = with pkgs; [ wakeonlan ];
-  
-  systemd.services.wol-lgtv = 
-  {
-    description = "WoL LGTV";
-    
-    wants = [ "network-online.target" ];
-    after = [ "network-online.target" "display-manager.service" ];
-    wantedBy = [ "graphical.target" ];
-    
-    serviceConfig = 
-    {
-      Type = "oneshot";
-      ExecStart = "${pkgs.wakeonlan}/bin/wakeonlan wakeonlan -i 192.168.30.6 60:45:e8:1e:b5:40"; 
-    };
-  };
-  
   services.xserver.xkb =
   {
     layout = "de";
