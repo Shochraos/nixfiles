8998cc72e9bc135a071eb40cd7e2b84f2ebdd961
Shochraos <github@shonline.slmail.me>
2026-04-16 00:31:41 +0200

    refactor

--- hosts/Azazel/host-specific.nix
+++ hosts/Azazel/host-specific.nix
@@ -49,53 +49,53 @@
       settings = 
       {
         barConfigs = [
-            {
-              id = "default";
-              name = "Main Bar";
-              enabled = true;
-              visible = true;
-              position = 0;
-              bottomGap = -5;
-              innerPadding = 5;
-              spacing = 0;
-              transparency = 0;
-              screenPreferences = [ "all" ];
-        
-              leftWidgets = 
-              [
-                { id = "spacer"; enabled = true; size = 5; }
-                { id = "clock"; enabled = true; }
-                { id = "spacer"; enabled = true; size = 15; }
-                { id = "workspaceSwitcher"; enabled = true; }
-                { id = "runningApps"; enabled = true; }
-              ];
-        
-              centerWidgets = 
-              [
-                { id = "focusedWindow"; enabled = true; }
-              ];
-        
-              rightWidgets = 
-              [
-                { id = "notificationButton"; enabled = true; }
-                { id = "clipboard"; enabled = true; }
-                { id = "dankKDEConnect"; enabled = true; }
-                { id = "spacer"; enabled = true; size = 15; }
-                { id = "cpuUsage"; enabled = true; minimumWidth = true; }
-                { id = "cpuTemp"; enabled = true; minimumWidth = true; }
-                { id = "network_speed_monitor"; enabled = true; }
-                { id = "spacer"; enabled = true; size = 15; }
-                { id = "systemTray"; enabled = true; }
-                { id = "controlCenterButton"; enabled = true; }
-                { id = "spacer"; enabled = true; size = 5; }
-              ];
-            }
-          ];
-      };
+          {
+            id = "default";
+            name = "Main Bar";
+            enabled = true;
+            visible = true;
+            position = 0;
+            bottomGap = -5;
+            innerPadding = 5;
+            spacing = 0;
+            transparency = 0;
+            screenPreferences = [ "all" ];
       
-      # Power menu
-      powerMenuActions = [ "reboot" "poweroff" ];
-      powerMenuDefaultAction = "poweroff";
+            leftWidgets = 
+            [
+              { id = "spacer"; enabled = true; size = 5; }
+              { id = "clock"; enabled = true; }
+              { id = "spacer"; enabled = true; size = 15; }
+              { id = "workspaceSwitcher"; enabled = true; }
+              { id = "runningApps"; enabled = true; }
+            ];
+      
+            centerWidgets = 
+            [
+              { id = "focusedWindow"; enabled = true; }
+            ];
+      
+            rightWidgets = 
+            [
+              { id = "notificationButton"; enabled = true; }
+              { id = "clipboard"; enabled = true; }
+              { id = "dankKDEConnect"; enabled = true; }
+              { id = "spacer"; enabled = true; size = 15; }
+              { id = "cpuUsage"; enabled = true; minimumWidth = true; }
+              { id = "cpuTemp"; enabled = true; minimumWidth = true; }
+              { id = "network_speed_monitor"; enabled = true; }
+              { id = "spacer"; enabled = true; size = 15; }
+              { id = "systemTray"; enabled = true; }
+              { id = "controlCenterButton"; enabled = true; }
+              { id = "spacer"; enabled = true; size = 5; }
+            ];
+          }
+        ];
+        
+        # Power menu
+        powerMenuActions = [ "reboot" "poweroff" ];
+        powerMenuDefaultAction = "poweroff";
+      };
     };
   };
 }
