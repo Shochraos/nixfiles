4d1c218db4a01ca8e6a912be7dc51fd4fd0ef7dc
Shochraos <github@shonline.slmail.me>
2026-04-16 00:24:29 +0200

    moved dms bar config to host-specific

--- hosts/Azazel/host-specific.nix
+++ hosts/Azazel/host-specific.nix
@@ -43,13 +43,54 @@
         };
     };
     
-    programs.ghostty =
+    programs.dank-material-shell = 
     {
-        settings =
-        { 
-          window-height = 50;
-          window-width  = 150;
-        };
+      settings = 
+      {
+        barConfigs = [
+            {
+              id = "default";
+              name = "Main Bar";
+              enabled = true;
+              visible = true;
+              position = 0;
+              bottomGap = -5;
+              innerPadding = 5;
+              spacing = 0;
+              transparency = 0;
+              screenPreferences = [ "all" ];
+        
+              leftWidgets = 
+              [
+                { id = "spacer"; enabled = true; size = 5; }
+                { id = "clock"; enabled = true; }
+                { id = "spacer"; enabled = true; size = 15; }
+                { id = "workspaceSwitcher"; enabled = true; }
+                { id = "runningApps"; enabled = true; }
+              ];
+        
+              centerWidgets = 
+              [
+                { id = "focusedWindow"; enabled = true; }
+              ];
+        
+              rightWidgets = 
+              [
+                { id = "notificationButton"; enabled = true; }
+                { id = "clipboard"; enabled = true; }
+                { id = "dankKDEConnect"; enabled = true; }
+                { id = "spacer"; enabled = true; size = 15; }
+                { id = "cpuUsage"; enabled = true; minimumWidth = true; }
+                { id = "cpuTemp"; enabled = true; minimumWidth = true; }
+                { id = "network_speed_monitor"; enabled = true; }
+                { id = "spacer"; enabled = true; size = 15; }
+                { id = "systemTray"; enabled = true; }
+                { id = "controlCenterButton"; enabled = true; }
+                { id = "spacer"; enabled = true; size = 5; }
+              ];
+            }
+          ];
+      };
     };
   };
 }
