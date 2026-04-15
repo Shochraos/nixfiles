d32ff92839f40ce3dcd87aa295b1836e5525fde3
Shochraos <github@shonline.slmail.me>
2026-04-15 23:48:00 +0200

    spotify font fix and additional calendar for nextcloud

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,5 +1,11 @@
-  { username, ... }:
+  { pkgs, username, ... }:
   {
+    fonts.packages = with pkgs; 
+    [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+    ];
+    
     home-manager.users.${username} =
     {
       fonts.fontconfig.enable = true;
