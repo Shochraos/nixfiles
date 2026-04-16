340c895d78b65b5c2c0060301d42a1d8e68656b8
Shochraos <github@raiton.net>
2026-04-16 15:37:03 +0200

    refactor

--- hosts/Solas/host-specific.nix
+++ hosts/Solas/host-specific.nix
@@ -2,6 +2,8 @@
 {
   services.upower.enable = true;
   services.fwupd.enable = true;
+  
+  systemd.network.wait-online.enable = false;
 
   hardware.alsa.enablePersistence = true;
 
--- modules/addons/laptop-networking.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  environment.etc = {
-    "ssl/certs/T-TeleSec_GlobalRoot_Class_2.pem".source =
-      ../../assets/certs/T-TeleSec_GlobalRoot_Class_2.pem;
-  };
-
-  systemd.network.wait-online.enable = false;
-}
