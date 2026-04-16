1635b21da7fdfae9fd35785ea71d4b7072ec3a21
Shochraos <github@raiton.net>
2026-04-16 18:19:02 +0200

    refactored and finished hyprland workspace configuration

--- hosts/Azazel/host-specific.nix
+++ hosts/Azazel/host-specific.nix
@@ -1,4 +1,8 @@
-{ pkgs, username, ... }:
+{
+  pkgs,
+  username,
+  ...
+}:
 {
   services.xserver.xkb = {
     layout = "de";
@@ -6,149 +10,164 @@
   };
   console.keyMap = "de";
 
-  home-manager.users.${username} = {
-    home.packages = with pkgs; [
-      feather
-      electrum
-    ];
-
-    xdg.autostart = {
-      entries = [
-        "${pkgs.discord.desktopItem}/share/applications/discord.desktop"
-        "${pkgs.spotify}/share/applications/spotify.desktop"
+  home-manager.users.${username} =
+    { config, ... }:
+    {
+      home.packages = with pkgs; [
+        feather
+        electrum
       ];
-    };
 
-    wayland.windowManager.hyprland = {
-      settings = {
-        input = {
-          kb_layout = "de";
-          kb_variant = "nodeadkeys";
+      xdg.autostart = {
+        entries = [
+          "${pkgs.discord.desktopItem}/share/applications/discord.desktop"
+          "${pkgs.spotify}/share/applications/spotify.desktop"
+          "${config.programs.zed-editor.package}/share/applications/dev.zed.Zed.desktop"
+        ];
+      };
 
-          accel_profile = "flat";
+      wayland.windowManager.hyprland = {
+        settings = {
+          input = {
+            kb_layout = "de";
+            kb_variant = "nodeadkeys";
+
+            accel_profile = "flat";
+          };
+
+          cursor = {
+            no_hardware_cursors = true;
+          };
+
+          windowrule = [
+            "match:class ^(zen-beta)$, workspace 2 silent"
+            "match:class ^(dev.zed.Zed)$, workspace 3 silent"
+            "match:class ^(discord)$, workspace 4 silent"
+            "match:class ^(spotify)$, workspace 5 silent"
+            "match:class ^(steam)$, workspace 5 silent"
+            "match:class ^(steam)$, match:title ^(Steam Settings)$, float on"
+
+            # Arknights Endfield
+            "match:class ^(endfield.exe)$, match:title ^(Form)$, float on, suppress_event maximize fullscreen activatefocus, fullscreen_state 0 0, workspace 3 silent"
+          ];
         };
+      };
 
-        cursor = {
-          no_hardware_cursors = true;
+      # DankBar
+      programs.dank-material-shell = {
+        settings = {
+          barConfigs = [
+            {
+              id = "default";
+              name = "Main Bar";
+              enabled = true;
+              visible = true;
+              position = 0;
+              bottomGap = -5;
+              innerPadding = 5;
+              spacing = 0;
+              transparency = 0;
+              screenPreferences = [ "all" ];
+
+              borderEnabled = false;
+              widgetOutlineEnabled = true;
+              widgetOutlineColor = "primary";
+              widgetOutlineThickness = 1;
+              widgetOutlineOpacity = 0.35;
+
+              leftWidgets = [
+                {
+                  id = "spacer";
+                  enabled = true;
+                  size = 5;
+                }
+                {
+                  id = "clock";
+                  enabled = true;
+                }
+                {
+                  id = "spacer";
+                  enabled = true;
+                  size = 15;
+                }
+                {
+                  id = "workspaceSwitcher";
+                  enabled = true;
+                }
+                {
+                  id = "runningApps";
+                  enabled = true;
+                }
+              ];
+
+              centerWidgets = [
+                {
+                  id = "focusedWindow";
+                  enabled = true;
+                }
+              ];
+
+              rightWidgets = [
+                {
+                  id = "notificationButton";
+                  enabled = true;
+                }
+                {
+                  id = "clipboard";
+                  enabled = true;
+                }
+                {
+                  id = "dankKDEConnect";
+                  enabled = true;
+                }
+                {
+                  id = "spacer";
+                  enabled = true;
+                  size = 15;
+                }
+                {
+                  id = "cpuUsage";
+                  enabled = true;
+                  minimumWidth = true;
+                }
+                {
+                  id = "cpuTemp";
+                  enabled = true;
+                  minimumWidth = true;
+                }
+                {
+                  id = "network_speed_monitor";
+                  enabled = true;
+                }
+                {
+                  id = "spacer";
+                  enabled = true;
+                  size = 15;
+                }
+                {
+                  id = "systemTray";
+                  enabled = true;
+                }
+                {
+                  id = "controlCenterButton";
+                  enabled = true;
+                }
+                {
+                  id = "spacer";
+                  enabled = true;
+                  size = 5;
+                }
+              ];
+            }
+          ];
+
+          # Power menu
+          powerMenuActions = [
+            "reboot"
+            "poweroff"
+          ];
+          powerMenuDefaultAction = "poweroff";
         };
       };
     };
-
-    # DankBar
-    programs.dank-material-shell = {
-      settings = {
-        barConfigs = [
-          {
-            id = "default";
-            name = "Main Bar";
-            enabled = true;
-            visible = true;
-            position = 0;
-            bottomGap = -5;
-            innerPadding = 5;
-            spacing = 0;
-            transparency = 0;
-            screenPreferences = [ "all" ];
-
-            borderEnabled = false;
-            widgetOutlineEnabled = true;
-            widgetOutlineColor = "primary";
-            widgetOutlineThickness = 1;
-            widgetOutlineOpacity = 0.35;
-
-            leftWidgets = [
-              {
-                id = "spacer";
-                enabled = true;
-                size = 5;
-              }
-              {
-                id = "clock";
-                enabled = true;
-              }
-              {
-                id = "spacer";
-                enabled = true;
-                size = 15;
-              }
-              {
-                id = "workspaceSwitcher";
-                enabled = true;
-              }
-              {
-                id = "runningApps";
-                enabled = true;
-              }
-            ];
-
-            centerWidgets = [
-              {
-                id = "focusedWindow";
-                enabled = true;
-              }
-            ];
-
-            rightWidgets = [
-              {
-                id = "notificationButton";
-                enabled = true;
-              }
-              {
-                id = "clipboard";
-                enabled = true;
-              }
-              {
-                id = "dankKDEConnect";
-                enabled = true;
-              }
-              {
-                id = "spacer";
-                enabled = true;
-                size = 15;
-              }
-              {
-                id = "cpuUsage";
-                enabled = true;
-                minimumWidth = true;
-              }
-              {
-                id = "cpuTemp";
-                enabled = true;
-                minimumWidth = true;
-              }
-              {
-                id = "network_speed_monitor";
-                enabled = true;
-              }
-              {
-                id = "spacer";
-                enabled = true;
-                size = 15;
-              }
-              {
-                id = "systemTray";
-                enabled = true;
-              }
-              {
-                id = "controlCenterButton";
-                enabled = true;
-              }
-              {
-                id = "spacer";
-                enabled = true;
-                size = 5;
-              }
-            ];
-          }
-        ];
-
-        # Power menu
-        powerMenuActions = [
-          "reboot"
-          "poweroff"
-        ];
-        powerMenuDefaultAction = "poweroff";
-      };
-    };
-  };
 }
--- modules/addons/common-packages.nix
+++ modules/addons/common-packages.nix
@@ -33,25 +33,5 @@
         ];
         customColorScheme = builtins.fromJSON (builtins.readFile ../../local/themes/spicetify.json);
       };
-
-    wayland.windowManager.hyprland = {
-      settings = {
-        bindr = [
-          # Discord
-          ", F9, sendshortcut, CTRL SHIFT, M, class:^(discord)$"
-          ", F10, sendshortcut, CTRL SHIFT, D, class:^(discord)$"
-
-          # Spotify
-          ", XF86AudioPrev, exec, playerctl --player=spotify previous"
-          ", XF86AudioPlay, exec, playerctl --player=spotify play-pause"
-          ", XF86AudioNext, exec, playerctl --player=spotify next"
-        ];
-        binde = [
-          # Spotify
-          ", F11, exec, playerctl --player=spotify volume 0.05-"
-          ", F12, exec, playerctl --player=spotify volume 0.05+"
-        ];
-      };
-    };
   };
 }
