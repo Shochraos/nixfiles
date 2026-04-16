07ee217ea903e8c7b8928f04dd2263657923a2da
Shochraos <github@raiton.net>
2026-04-16 16:53:42 +0200

    added binary cache to nix cachyos

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -3,7 +3,12 @@
   boot.loader.systemd-boot.enable = true;
 
   boot.loader.efi.canTouchEfiVariables = true;
-
+ 
+  # nix-cachyos-kernel binary-cache
+  nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
+  nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ]; 
+  
+  # nix-cachyos-kernel
   nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-zen4;
 }
