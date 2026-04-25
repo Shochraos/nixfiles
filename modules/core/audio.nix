bd9aa1bebcba0e2716be770157b3c17b07c5b3b8
Shochraos <github@shonline.slmail.me>
2026-04-25 22:31:43 +0200

    fixed rnnnoise

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -7,43 +7,45 @@
     alsa.enable = true;
     alsa.support32Bit = true;
 
-    # extraConfig.pipewire."99-input-denoising" = {
-    #   "context.modules" = [
-    #     {
-    #       name = "libpipewire-module-filter-chain";
-    #       args = {
-    #         "node.description" = "RNNoise";
-    #         "media.name" = "RNNoise";
-    #         "filter.graph" = {
-    #           nodes = [
-    #             {
-    #               type = "ladspa";
-    #               name = "rnnoise";
-    #               plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-    #               label = "noise_suppressor_mono";
-    #               control = {
-    #                 "VAD Threshold (%)" = 50.0;
-    #                 "VAD Grace Period (ms)" = 200;
-    #                 "Retroactive VAD Grace (ms)" = 0;
-    #               };
-    #             }
-    #           ];
-    #         };
-    #         "capture.props" = {
-    #           "node.name" = "capture.rnnoise_source";
-    #           "node.passive" = true;
-    #           "audio.rate" = 48000;
-    #           "audio.position" = "[ MONO ]";
-    #         };
-    #         "playback.props" = {
-    #           "node.name" = "rnnoise_source";
-    #           "media.class" = "Audio/Source";
-    #           "audio.rate" = 48000;
-    #           "audio.position" = "[ MONO ]";
-    #         };
-    #       };
-    #     }
-    #   ];
-    # };
+    extraLadspaPackages = [ pkgs.rnnoise-plugin ];
+    
+    extraConfig.pipewire."99-input-denoising" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-filter-chain";
+          args = {
+            "node.description" = "RNNoise";
+            "media.name" = "RNNoise";
+            "filter.graph" = {
+              nodes = [
+                {
+                  type = "ladspa";
+                  name = "rnnoise";
+                  plugin = "librnnoise_ladspa";
+                  label = "noise_suppressor_mono";
+                  control = {
+                    "VAD Threshold (%)" = 50.0;
+                    "VAD Grace Period (ms)" = 200;
+                    "Retroactive VAD Grace (ms)" = 0;
+                  };
+                }
+              ];
+            };
+            "capture.props" = {
+              "node.name" = "capture.rnnoise_source";
+              "node.passive" = true;
+              "audio.rate" = 48000;
+              "audio.position" = "[ MONO ]";
+            };
+            "playback.props" = {
+              "node.name" = "rnnoise_source";
+              "media.class" = "Audio/Source";
+              "audio.rate" = 48000;
+              "audio.position" = "[ MONO ]";
+            };
+          };
+        }
+      ];
+    };
   };
 }
