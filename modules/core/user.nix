463379ffb7d2b669e89c8c3bb42e19ce9faa2a71
Shochraos <github@shonline.slmail.me>
2026-04-25 23:47:41 +0200

    adjusted update command to apply config on boot and not on switch

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -51,7 +51,7 @@
 
         nh-switch = "nh os switch --ask path:/home/${username}/Repositories/nixfiles -H ${systemname}";
         nh-boot = "nh os boot --ask path:/home/${username}/Repositories/nixfiles -H ${systemname}";
-        nh-update = "nh os switch -u --ask path:/home/${username}/Repositories/nixfiles -H ${systemname}";
+        nh-update = "nh os boot -u --ask path:/home/${username}/Repositories/nixfiles -H ${systemname}";
 
         copy = "dms cl copy $argv";
         paste = "dms cl paste";
