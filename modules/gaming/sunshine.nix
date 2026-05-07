825d0de37ed9d4f685b66fc518c1eb9e6fd56d19
Shochraos <github@shonline.slmail.me>
2026-05-07 16:59:35 +0200

    implemented sunshine moondeck configuration

--- modules/gaming/sunshine.nix
+++ modules/gaming/sunshine.nix
@@ -1,13 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
+let
+  moondeck = pkgs.callPackage ../../packages/moondeck-buddy { };
+in
 {
   services.sunshine = {
     enable = true;
+    package = pkgs.sunshine.override {
+      cudaSupport = true;
+      cudaPackages = pkgs.cudaPackages;
+    };
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+
+    applications = {
+      env = {
+        PATH = "$(PATH):/run/current-system/sw/bin:/etc/profiles/per-user/${username}/bin:$(HOME)/.local/bin";
+      };
+      apps = [
+        {
+          name = "MoonDeckStream";
+          cmd = "${moondeck}/bin/MoonDeckStream";
+          exclude-global-prep-cmd = "false";
+          elevated = "false";
+        }
+      ];
+    };
   };
 
-  environment.systemPackages = [
-      (pkgs.callPackage ../packages/moondeck-buddy { })
-  ];
+  home-manager.users.${username} = {
+    home.packages = [ moondeck ];
+    xdg.autostart.entries = [ "${moondeck}/share/applications/MoonDeckBuddy.desktop" ];
+  };
+  
+  networking.firewall.allowedTCPPorts = [ 59999 ];
 }
