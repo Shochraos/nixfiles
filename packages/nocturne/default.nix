19af0622fd3328bf5b13c17b9e93d4fb7911c09d
Shochraos <github@shonline.slmail.me>
2026-05-22 11:18:00 +0200

    updated nocturne package

--- packages/nocturne/default.nix
+++ packages/nocturne/default.nix
@@ -11,6 +11,7 @@
   gobject-introspection,
   wrapGAppsHook4,
   glib,
+  glib-networking,
   libadwaita,
   libsecret,
   gtk4,
@@ -19,6 +20,27 @@
   shared-mime-info,
 }:
 
+let
+  mpris_server = python3.pkgs.buildPythonPackage rec {
+    pname = "mpris_server";
+    version = "0.9.6";
+    format = "setuptools";
+
+    src = python3.pkgs.fetchPypi {
+      inherit pname version;
+      hash = "sha256-T0ZeDQiYIAhKR8aw3iv3rtwzc+R0PTQuIh6+Hi4rIHQ=";
+    };
+
+    propagatedBuildInputs = [
+      python3.pkgs.strenum
+      python3.pkgs.pydbus
+      python3.pkgs.emoji
+      python3.pkgs.unidecode
+    ];
+
+    doCheck = false;
+  };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "nocturne";
   version = "1.0.1";
@@ -43,8 +65,22 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   buildInputs = [
-    (python3.withPackages (ps: [ ps.pygobject3 ]))
+    (python3.withPackages (ps: [
+      ps.pygobject3
+      ps.tinytag
+      ps.requests
+      ps.urllib3
+      ps.pillow
+      ps.strenum
+      ps.pydbus
+      ps.emoji
+      ps.unidecode
+      ps.syncedlyrics
+      ps.colorthief
+      mpris_server
+    ]))
     glib
+    glib-networking
     gtk4
     libadwaita
     libsecret
@@ -53,6 +89,8 @@ stdenv.mkDerivation (finalAttrs: {
     gst-plugins-base
     gst-plugins-good
     gst-plugins-bad
+    gst-plugins-ugly
+    gst-libav
   ]);
 
   meta = with lib; {
