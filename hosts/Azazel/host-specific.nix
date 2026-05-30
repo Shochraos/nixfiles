f5d5042d0ced6fc69a3dd421cd79a2f12e1a279a
Shochraos <github@shonline.slmail.me>
2026-05-30 18:03:37 +0200

    revert of spacing change due to different monitor

--- hosts/Azazel/host-specific.nix
+++ hosts/Azazel/host-specific.nix
@@ -33,7 +33,7 @@
               position = 0;
               bottomGap = -5;
               innerPadding = 5;
-              spacing = 0;
+              spacing = 2;
               transparency = 0;
 
               screenPreferences = [ "all" ];
