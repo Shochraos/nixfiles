f5aa9a1ab91379973f1bc9dc051414e0b7384690
Shochraos <github@shonline.slmail.me>
2026-07-12 14:57:06 +0200

    refactored layer rules

--- modules/desktop/hypr-animations.nix
+++ modules/desktop/hypr-animations.nix
@@ -81,40 +81,6 @@
           style = "popin 80%";
         }
       ];
-
-      layer_rule = [
-        {
-          match = {
-            namespace = "^(dms.*)$";
-          };
-          ignore_alpha = 0.0;
-          blur = true;
-        }
-        {
-          match = {
-            namespace = "^(dms:(clipboard|file-browser|settings|spotlight|bluetooth-pairing|color-picker|hyprkeybinds|network-info|network-info-wired|notification-center-modal|polkit|power-menu|process-list-modal|wifi-password|confirm-modal|modal))$";
-          };
-          dim_around = 1;
-        }
-        {
-          match = {
-            namespace = "^(dms:bar)$";
-          };
-          no_anim = true;
-        }
-        {
-          match = {
-            namespace = "^(dms:(app-launcher|control-center|clipboard-popout|battery|vpn|dash|notification-center-popout|process-list-popout|popout|plugins:.*|plugins:plugin))$";
-          };
-          animation = "slide top";
-        }
-        {
-          match = {
-            namespace = "^(dms:(clipboard|file-browser|settings|spotlight|bluetooth-pairing|color-picker|hyprkeybinds|network-info|network-info-wired|notification-center-modal|polkit|power-menu|process-list-modal|wifi-password|confirm-modal|modal))$";
-          };
-          animation = "popin 80%";
-        }
-      ];
     };
   };
 }
--- modules/desktop/hypr-rules.nix
+++ modules/desktop/hypr-rules.nix
@@ -36,6 +36,40 @@
           }
         ]
         ++ osConfig.host.hyprland.windowRules;
+
+        layer_rule = [
+          {
+            match = {
+              namespace = "^(dms.*)$";
+            };
+            ignore_alpha = 0;
+            blur = true;
+          }
+          {
+            match = {
+              namespace = "^(dms:(clipboard|file-browser|settings|spotlight|bluetooth-pairing|color-picker|hyprkeybinds|network-info|network-info-wired|notification-center-modal|polkit|power-menu|process-list-modal|wifi-password|confirm-modal|modal))$";
+            };
+            dim_around = 1;
+          }
+          {
+            match = {
+              namespace = "^(dms:bar)$";
+            };
+            no_anim = true;
+          }
+          {
+            match = {
+              namespace = "^(dms:(app-launcher|control-center|clipboard-popout|battery|vpn|dash|notification-center-popout|process-list-popout|popout|plugins:.*|plugins:plugin))$";
+            };
+            animation = "slide top";
+          }
+          {
+            match = {
+              namespace = "^(dms:(clipboard|file-browser|settings|spotlight|bluetooth-pairing|color-picker|hyprkeybinds|network-info|network-info-wired|notification-center-modal|polkit|power-menu|process-list-modal|wifi-password|confirm-modal|modal))$";
+            };
+            animation = "popin 80%";
+          }
+        ];
       };
     };
 }
