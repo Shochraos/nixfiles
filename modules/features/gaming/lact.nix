8109a45f6cf32c7439aeeec41fbd024fec1a2e3a
Shochraos <github@shonline.slmail.me>
2026-07-29 12:28:21 +0200

    temporary lact fix until pr is merged

--- modules/features/gaming/lact.nix
+++ modules/features/gaming/lact.nix
@@ -1,7 +1,14 @@
+{ inputs, ... }:
+let
+  lactBuildFixOverlay = final: prev: {
+    inherit (inputs.nixpkgs-small.legacyPackages.${prev.stdenv.hostPlatform.system}) lact;
+  };
+in
 {
   den.aspects.gaming.nixos =
     { pkgs, ... }:
     {
+      nixpkgs.overlays = [ lactBuildFixOverlay ];
       environment.systemPackages = with pkgs; [ lact ];
       systemd = {
         packages = with pkgs; [ lact ];
