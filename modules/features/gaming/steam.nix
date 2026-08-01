b251aa7e9954f5aabd38de96c46dad9d54e20d5a
Shochraos <github@raiton.net>
2026-08-01 12:10:15 +0200

    switched from default proton preset to k

--- modules/features/gaming/steam.nix
+++ modules/features/gaming/steam.nix
@@ -33,6 +33,7 @@
         PROTON_ENABLE_WAYLAND = "1";
         PROTON_DLSS_UPGRADE = "1";
         PROTON_VKD3D_LOWLATENCY = "1";
+        DXVK_NVAPI_DRS_SETTINGS = "ngx_dlss_sr_override=on,ngx_dlss_sr_override_render_preset_selection=render_preset_l,ngx_dlss_rr_override=on,ngx_dlss_rr_override_render_preset_selection=render_preset_l";
         VKD3D_CONFIG = "descriptor_heap";
         STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${proton-cachyos}/share/steam/compatibilitytools.d:${dw-proton}/share/steam/compatibilitytools.d";
       };
