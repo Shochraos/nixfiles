20c95c7a4310ddbc179dc8cc5b2287851904388b
Shochraos <github@shonline.slmail.me>
2026-08-06 14:39:52 +0200

    temporarily updated nvidia and hyprland

--- modules/desktop/hyprland/config.nix
+++ modules/desktop/hyprland/config.nix
@@ -1,7 +1,20 @@
+let
+  hyprlandGlazeRelaxOverlay = _final: prev: {
+    hyprland = prev.hyprland.overrideAttrs (old: {
+      postPatch = ''
+        substituteInPlace CMakeLists.txt start/CMakeLists.txt hyprpm/CMakeLists.txt \
+          --replace-fail "glaze 7...<8" "glaze"
+      ''
+      + old.postPatch;
+    });
+  };
+in
 {
   den.aspects.hyprland.nixos =
     { pkgs, ... }:
     {
+      nixpkgs.overlays = [ hyprlandGlazeRelaxOverlay ];
+
       environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
       programs.hyprland = {
