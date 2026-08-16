46619b340a202896e66cb5f5aa6eb9666821b106
Shochraos <github@shonline.slmail.me>
2026-08-16 14:24:46 +0200

    changed driver to nvidia production

--- modules/features/nvidia.nix
+++ modules/features/nvidia.nix
@@ -23,7 +23,7 @@
         powerManagement.finegrained = false;
         open = true;
         nvidiaSettings = false;
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+        package = config.boot.kernelPackages.nvidiaPackages.production;
       };
     };
 
